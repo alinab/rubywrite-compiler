176ca558a43943449aef7f234826af8062d2b399
Alina Banerjee <banerjea@indiana.edu>
2012-02-07 18:11:02 -0500

    Updated compound_if_3
    Test case now checks for if-else blocks with
    multiple statements

--- test/parsing/compound_if_3.c
+++ test/parsing/compound_if_3.c
@@ -1,13 +1,19 @@
 int main() 
 {
   int b;
+  int k;
   a = 3;
   c = 20;
   b = a + c;
   if (b + c < b + a) {
+    k = 3;
+    k = k + b;
     printf("b is %d\n",b);
+
   }
   else {
     printf("a is %d and c is %d\n");
+    printf(k);
+    printf("end\n");
   }
 }
